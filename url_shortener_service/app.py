20f4185adc50f79ee8571cfa816287e583e0be9a
suman2215 <sumanchouhan0313@gmail.com>
2024-08-25 00:16:10 +0530

    Add URL Shortener Microservice
    Signed-off-by: suman2215 <sumanchouhan0313@gmail.com>

---
+++ url_shortener_service/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+import string, random
+
+app = Flask(__name__)
+
+# MongoDB setup
+client = MongoClient("mongodb://mongodb:27017/")
+db = client.urlshortener
+url_collection = db.urls
+
+def generate_short_id(num_of_chars=6):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=num_of_chars))
+
+@app.route('/shorten', methods=['POST'])
+def shorten_url():
+    original_url = request.json['url']
+    short_id = generate_short_id()
+    url_collection.insert_one({'original_url': original_url, 'short_id': short_id})
+    return jsonify({"shortened_url": f"http://{request.host}/{short_id}"}), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
