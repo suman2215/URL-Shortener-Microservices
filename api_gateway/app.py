26ed11a849c20efef4e8c250f02b3df89fbb9e1d
suman2215 <sumanchouhan0313@gmail.com>
2024-08-25 00:15:05 +0530

    Add API Gateway Microservice
    Signed-off-by: suman2215 <sumanchouhan0313@gmail.com>

---
+++ api_gateway/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+# URL Shortening Route
+@app.route('/shorten', methods=['POST'])
+def shorten_url():
+    response = requests.post('http://url-shortener-service:5001/shorten', json=request.json)
+    return jsonify(response.json()), response.status_code
+
+# URL Redirection Route
+@app.route('/<short_id>', methods=['GET'])
+def redirect_url(short_id):
+    response = requests.get(f'http://url-redirection-service:5002/{short_id}')
+    return response.content, response.status_code
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
